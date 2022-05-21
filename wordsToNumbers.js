06ba2d4c7ef0e8ec62fd729eddf1bde2d143d5d2
Mori <sayedmurtazamuttahary@gmail.com>
2022-05-21 08:24:29 +0200

    last changes

--- wordsToNumbers.js
+++ wordsToNumbers.js
@@ -13,7 +13,7 @@ const convertToNumber = (word, { b, c, z, df, sp } = {
     sp: word.toLowerCase().split(/-|\s+and\s+|\s+/gi)
 }) => Object.keys(z).reduce((acc, k) => acc + sp
     .splice(0, !z[k] ? sp.length : sp.indexOf(k.replace(/\$/, 'illion')) + 1)
-    .map(w => !b.includes(w) ? c[w] ?? 10 ** z[w.replace(/illion$/, '$')] : b.indexOf(w))
+    .map(w => !b.includes(w) ? c[w] || 10 ** z[w.replace(/illion$/, '$')] : b.indexOf(w))
     .reduce((ac, n, i, arr) => df(n, arr[i - 1], i) ? ac * n : ac + n, 0), 0, '') || -1;
 
 const examples = [
@@ -21,18 +21,20 @@ const examples = [
     ["twenty", 20],
     ["one hundred and forty two", 142],
     ["one hundred and eleven", 111],
-    ["three thousand five hundred", 3500],
-    ["three thousand five hundred and seventy two", 3572],
-    ["three thousand five hundred and fourteen", 3514],
-    ["three hundred thousand six hundred", 300600],
-    ["three hundred thousand six hundred and eleven", 300611],
-    ["three hundred eleven thousand six hundred and twenty-two", 311622],
-    ["five hundred million three hundred thousand and twenty", 500300020],
-    ["five hundred forty two million three hundred thousand and twenty", 542300020],
-    ["five hundred million three hundred thirty two thousand one hundred twenty", 500332120],
-    ["ninety-four trillion eight hundred thirty billion three hundred eighty-four million thirty-two thousand nine hundred eighty-two", 94830384032982],
-    ["ninety-four quintillion eight hundred thirty quadrillion three hundred eighty-four trillion thirty-two billion nine hundred eighty-two million two hundred thirty-five thousand two hundred thirty-four", 94830384032982235234],
-    ["five hundred ninety-seven sextillion four hundred thirty-eight quintillion forty-five quadrillion eight hundred forty trillion three hundred eighty-three billion two hundred forty-five million one hundred twenty-three thousand four hundred forty-three", 597438045840383245123443]
+    ["three thousand five hundred", 3_500],
+    ["three thousand five hundred and seventy two", 3_572],
+    ["three thousand five hundred and fourteen", 3_514],
+    ["three hundred thousand six hundred", 300_600],
+    ["three hundred thousand six hundred and eleven", 300_611],
+    ["three hundred eleven thousand six hundred and twenty-two", 311_622],
+    ["five hundred million three hundred thousand and twenty", 500_300_020],
+    ["five hundred forty two million three hundred thousand and twenty", 542_300_020],
+    ["five hundred million three hundred thirty two thousand one hundred twenty", 500_332_120],
+    ["ninety-four trillion eight hundred thirty billion three hundred eighty-four million thirty-two thousand nine hundred eighty-two", 94_830_384_032_982],
+    ["ninety-four quintillion eight hundred thirty quadrillion three hundred eighty-four trillion thirty-two billion nine hundred eighty-two million two hundred thirty-five thousand two hundred thirty-four", 94_830_384_032_982_235_234],
+    ["five hundred ninety-seven sextillion four hundred thirty-eight quintillion forty-five quadrillion eight hundred forty trillion three hundred eighty-three billion two hundred forty-five million one hundred twenty-three thousand four hundred forty-three", 597_438_045_840_383_245_123_443]
 ]
 
-examples.forEach(ex => console.assert(convertToNumber(ex[0]) === ex[1]));
+examples.forEach(ex => {
+    console.assert(convertToNumber(ex[0]) === ex[1]);
+});
